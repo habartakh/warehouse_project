f7b647c7119d3e48b5f15bb0593e3a65ba5a8af7
Hajar Bartakh <bartakh.hajar@gmail.com>
2025-05-16 20:08:15 +0000

    Added package for robot localization

---
+++ localization_server/launch/localization.launch.py
@@ -0,0 +1,95 @@
+import os
+import launch
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+from launch.substitutions import PythonExpression
+
+def generate_launch_description():
+    # Name of the current package
+    package_description = 'localization_server'
+
+    # Name of the file containing the map and its path
+    map_file_arg =  launch.actions.DeclareLaunchArgument('map_file', default_value='warehouse_map_sim.yaml')
+    map_file_f = LaunchConfiguration('map_file')
+
+    map_file = PathJoinSubstitution([
+        get_package_share_directory('map_server'),
+        'config',
+        map_file_f
+    ])    
+    
+    # The path to the config file containing the parameters for the AMCL algorithm
+    amcl_config_sim = "amcl_config_sim.yaml"
+    amcl_config_real = "amcl_config_real.yaml"
+
+   # Use PythonExpression to choose the full YAML path
+    nav2_yaml = PythonExpression([
+        '"', os.path.join(get_package_share_directory(package_description), 'config', amcl_config_sim), 
+        '" if "', map_file_f, '" == "warehouse_map_sim.yaml" else "', 
+        os.path.join(get_package_share_directory(package_description), 'config', amcl_config_real), '"'
+    ])
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'amcl_rviz.rviz')
+
+
+    map_server_node = Node(
+                        package='nav2_map_server',
+                        executable='map_server',
+                        name='map_server',
+                        output='screen',
+                        parameters=[{'use_sim_time': True},
+                                    {'yaml_filename':map_file}
+                                ]
+                    )
+
+    amcl_node = Node(
+                    package='nav2_amcl',
+                    executable='amcl',
+                    name='amcl',
+                    output='screen',
+                    parameters=[nav2_yaml]          
+        )
+
+    lifecycle_manager_node = Node(
+                            package='nav2_lifecycle_manager',
+                            executable='lifecycle_manager',
+                            name='lifecycle_manager_mapper',
+                            output='screen',
+                            parameters=[{'use_sim_time': True},
+                                        {'autostart': True},
+                                        {'node_names': ['map_server','amcl']}]) 
+
+    rviz_node = Node(
+                package='rviz2',
+                executable='rviz2',
+                output='screen',
+                name='rviz_node',
+                parameters=[{'use_sim_time': True}],
+                arguments=['-d', rviz_config_dir])
+
+    # Event handler to launch map_server after rviz2 starts
+    delayed_map_server = RegisterEventHandler(
+        OnProcessStart(
+            target_action=rviz_node,
+            on_start=[  
+                        map_server_node,
+                        amcl_node,
+                        lifecycle_manager_node,
+                      ]
+        )
+    )
+
+    return LaunchDescription([
+        map_file_arg,
+        rviz_node,
+        # delayed_map_server,
+        lifecycle_manager_node,
+        map_server_node,
+        amcl_node,
+              
+        ])
